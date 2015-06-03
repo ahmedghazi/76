dd075b41ea57b093418ee57c92e8a682a9331321
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2015-06-03 16:05:23 +0200

    diapo video

--- themes/76/_/inc/agence-metier.php
+++ themes/76/_/inc/agence-metier.php
@@ -17,36 +17,7 @@ id="art-<?php echo $slug;?>">
 		</div>
 		
 		<div class="slideRight anime_md abtn">
-
-			<?php 
-				$diapo = get_field("diaporama_id");
-				$video = get_field("video_url");
-				if($diapo || $video){
-			?>
-					<div class="actionsBtn ">
-						<?php
-						$diapo = get_field("diaporama_id");
-						if($diapo){
-							$diapo_label = get_field("diaporama_label"); ?>
-				
-							<button 
-							type="button" 
-							class="btn btn_diapo" 
-							data-href="diapo-<?php echo $diapo?>" 
-							data-surtitre="<?php the_field("surtitre");?>" 
-							data-titre="<?php the_title();?>"
-							><?php echo $diapo_label?></button>
-						<?php }?>
-
-						<?php
-						
-						if($video){
-							$video_label = get_field("video_label"); ?>
-				
-							<button type="button" class="btn btn_video" data-href="video-<?php echo $video?>"><?php echo $video_label?></button>
-						<?php }?>
-					</div>
-			<?php }?>
+			<?php include(locate_template('_/inc/btn-media.php'));?>
 		</div>
 		
 	</div>
