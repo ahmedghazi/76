e2c6299fab3c893cb0a43ed4bd0dd69f6f84c968
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2015-06-25 17:33:58 +0200

    scroll

--- themes/76mobile/_/inc/creation-intro.php
+++ themes/76mobile/_/inc/creation-intro.php
@@ -7,19 +7,13 @@ $slug = $post->post_name;
 ?><article class="creation_intro article_creation_<?php echo $c;?>" <?php echo $css;?>
 data-slug="<?php echo $slug;?>"
 id="art-<?php echo $slug;?>">
-	<div class="creation_content">
-		<div class="surtitre"><?php the_title(); ?></div>
-		<h2><?php the_field("soustitre"); ?></h2>
-
-		<div class="contenu">
-			<?php the_content(); ?>
-		</div>
-		
-		<?php $redirect = get_field("redirect");?>
+	
+	<?php $redirect = get_field("redirect");?>
 		<?php if($redirect){?>
 			<?php $redirect_label = get_field("redirect_label");?>
 			<div class="actionsBtn">
-				<a class="btn" data-role="hash" href="<?php echo $redirect; ?>">
+				
+				<a class="action" data-role="hash" href="<?php echo $redirect; ?>">
 					
 					<span class="anime">
 						<?php echo $redirect_label;?>
@@ -28,6 +22,17 @@ id="art-<?php echo $slug;?>">
 			</div>
 			
 		<?php } ?>
+	
+	
+	<div class="creation_content">
+		<div class="surtitre"><?php the_title(); ?></div>
+		<h2><?php the_field("soustitre"); ?></h2>
+
+		<div class="contenu">
+			<?php the_content(); ?>
+		</div>
+		
+		
 		
 		<div class="logo ">
 			<?php include(locate_template('_/img/logo-soixanteseize-blanc.svg'));?>
